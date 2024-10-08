266d089a1d7619f9fbd09ce1abaa9c1d30153626
caioH12 <141171838+caioH12@users.noreply.github.com>
2024-10-08 12:07:57 -0300

    a

---
+++ perguntas.js
@@ -0,0 +1,100 @@
+criaCartao(
+    'matematica',
+    'como se calcula a soma dos termos de uma P.A?',
+    'utiliza-se a fórmula: ((al+an).q)/2',
+)
+criaCartao(
+    'historia',
+    'Em que ano começou a ww2?',
+    'em 1939'
+)
+criaCartao(
+    'musica',
+    'quantas linhas tem uma pauta?',
+    '5'
+)
+criaCartao(
+    'musica',
+    'quem foi beethoven?',
+    'compositor alemão'
+)
+criaCartao(
+    'musica',
+    'quem foi bach?',
+    'compositor barroco'
+)
+criaCartao(
+    'musica',
+    'quantas linhas tem uma pauta?',
+    '5'
+)
+criaCartao(
+    'musica',
+    'quem foi paganini?',
+    'compositor italiano'
+)
+criaCartao(
+    'enem',
+    'o que cai no enem?',
+    'lagrimas'
+)
+criaCartao(
+    'quimica',
+    'quais elementos compõem a água?',
+    'hidrogênio e oxigênio'
+)
+criaCartao(
+    'copa do mundo',
+    'quem venceu a ultima copa do mundo?',
+    'argentina'
+)
+criaCartao(
+    'musica',
+    'qual a duração de uma breve?',
+    '8 tempos'
+)
+criaCartao(
+    'musica',
+    'qual a duração de uma semibreve?',
+    '4 tempos'
+)
+criaCartao(
+    'musica',
+    'qual a duração de uma minima?',
+    '2 tempos'
+)
+criaCartao(
+    'musica',
+    'qual a duração de uma seminima?',
+    '1 tempo'
+)criaCartao(
+    'musica',
+    'qual a duração de uma colcheia?',
+    '1/2 tempo'
+)
+criaCartao(
+    'musica',
+    'qual a duração de uma semicolcheia?',
+    '1/4 tempo'
+)
+criaCartao(
+    'musica',
+    'qual a duração de uma fusa?',
+    '1/8 tempo'
+)
+criaCartao(
+    'musica',
+    'qual a duração de uma semifusa?',
+    '1/16 tempo'
+)
+criaCartao(
+    'musica',
+    'qual a duração de uma quartifusa?',
+    '1/32 tempo'
+)
+criaCartao(
+    'musica',
+    'quantos sustenidos tem a escala de LÁ Maior?',
+    '3 sustenidos'
+)
+
