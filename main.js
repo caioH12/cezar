28dfbaea1bd610c563c1365a9adea8f2404e90be
caioH12 <141171838+caioH12@users.noreply.github.com>
2024-10-30 12:02:42 -0300

    t

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function criaCartao(categoria, pergunta, resposta){
+function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao';
@@ -13,13 +13,13 @@ function criaCartao(categoria, pergunta, resposta){
     </div>
 </div>
     `
-let repostaEstaVisivel = false;
+    let respostaEstaVisivel = false;
 
-function viraCartao(){
-    let repostaEstaViivel = !repostaEstaVisivel;
-    cartao.classList.toggle('active', repostaEstaViivel );
-}
-cartao.addEventListener('click',viraCartao);
+    function viraCartao() {
+        let respostaEstaVisivel = !respostaEstaVisivel;
+        cartao.classList.toggle('active', respostaEstaVisivel);
+    }
+    cartao.addEventListener('click', viraCartao);
 
     container.appendChild(cartao);
 }
